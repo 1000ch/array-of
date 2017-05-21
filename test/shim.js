033d7067e35c03784c7cab54dc029f4bd1d01874
1000ch <shogo.sensui@gmail.com>
2017-05-21 14:19:12 +0900

    Include all test of es6-shim

--- test/shim.js
+++ test/shim.js
@@ -1,12 +1,38 @@
-var of = require('../');
+require('../shim');
 var test = require('tape');
 var functionsHaveNames = (function foo() {}).name === 'foo';
 var ifFunctionsHaveNamesTest = functionsHaveNames ? test : test.skip;
 
+if (!Object.prototype.hasOwnProperty.call(Array, 'of')) {
+  return it('exists', function (t) {
+    t.same(!!Array.of, true);
+  });
+}
+
+ifFunctionsHaveNamesTest('has the correct name', function (t) {
+  t.plan(1);
+
+  t.same(Array.of.name, 'of');
+});
+
+test('has the right arity', function (t) {
+  t.plan(1);
+
+  t.same(Array.of.length, 0);
+});
+
+test('is not enumerable', function (t) {
+  t.plan(1);
+
+  var descriptor = Object.getOwnPropertyDescriptor(Array, 'of');
+
+  t.same(descriptor.enumerable, false);
+});
+
 test('should create correct array from arguments', function (t) {
   t.plan(1);
 
-  t.same(of(1, null, undefined), [1, null, undefined]);
+  t.true(Array.of(1, null, undefined), [1, null, undefined]);
 });
 
 test('should work with other constructors', function (t) {
@@ -22,8 +48,8 @@ test('should work with other constructors', function (t) {
     expected[index] = arg;
   });
 
-  t.true(of.apply(Foo, args) instanceof Foo);
-  t.same(of.apply(Foo, args), expected);
+  t.true(Array.of.apply(Foo, args) instanceof Foo);
+  t.same(Array.of.apply(Foo, args), expected);
 });
 
 test('still works without Array.from', function (t) {
@@ -31,6 +57,6 @@ test('still works without Array.from', function (t) {
 
   var originalFrom = Array.from;
   Array.from = 42;
-  t.same(of(1, 2, 3), [1, 2, 3]);
+  t.same(Array.of(1, 2, 3), [1, 2, 3]);
   Array.from = originalFrom;
 });
